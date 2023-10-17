d0ffc70acfdf04f14a8868465316c918f87ba763
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-10-17 14:44:17 +0200

    fix path issues

--- Fhir_Kotlin/core/src/main/kotlin/com/sevenreup/fhir/core/config/ProjectConfig.kt
+++ Fhir_Kotlin/core/src/main/kotlin/com/sevenreup/fhir/core/config/ProjectConfig.kt
@@ -5,6 +5,8 @@ import com.sevenreup.fhir.core.utils.getActualPath
 import com.sevenreup.fhir.core.utils.getParentPath
 import com.sevenreup.fhir.core.utils.readFileOrNull
 import com.sevenreup.fhir.core.utils.toAbsolutePath
+import java.nio.file.Paths
+import kotlin.io.path.Path
 
 class ProjectConfigManager {
     fun loadProjectConfig(projectRoot: String?, file: String?): ProjectConfig {
@@ -21,9 +23,10 @@ class ProjectConfigManager {
     }
 
     private fun searchForConfig(path: String): ProjectConfig {
-        println(path)
+        val actualPath = Paths.get(path,"fhir.compiler.json").normalize().toString()
+        println("$path - $actualPath")
         val gson = Gson()
-        val rawJson: String = "$path\\fhir.compiler.json".readFileOrNull() ?: return ProjectConfig()
+        val rawJson: String = actualPath.readFileOrNull() ?: return ProjectConfig()
         return gson.fromJson(rawJson, ProjectConfig::class.java).copy(basePath = path)
     }
 }
